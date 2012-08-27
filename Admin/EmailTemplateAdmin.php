dee14e4a5d90caeb7bf17a8a2447174226b1c8a5
Jeremy Marc <jeremy.marc@me.com>
2012-08-27 12:30:10 -0700

    [Admin] Sonata Admin files

---
+++ Admin/EmailTemplateAdmin.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Rj\EmailBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Admin\Admin;
+use Rj\EmailBundle\Form\Type\CallbackType;
+
+class EmailTemplateAdmin extends Admin
+{
+    protected $baseRouteName = 'email_template';
+    protected $baseRoutePattern = 'email_template';
+
+    //show
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    //add
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $type = new CallbackType(function($builder) {
+            $builder->add('fromName', 'text', array(
+                'label' => 'From (name)'
+            ));
+            $builder->add('fromEmail', 'email', array(
+                'label' => 'From (email)'
+            ));
+            $builder->add('subject', 'text', array(
+                'label' => 'Subject'
+            ));
+            $builder->add('body', 'textarea', array(
+                'label' => 'Body'
+            ));
+        });
+
+        $formMapper
+            ->with('Email Templates')
+                ->add('name')
+                ->add('translationProxies', 'collection', array(
+                    'type' => $type
+                ))
+            ->end()
+        ;
+    }
+
+    //list
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->addIdentifier('createdAt')
+            ->addIdentifier('updatedAt')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'view' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                    //'send' => array(),
+                )
+            ))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('name')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ;
+    }
+}
---
+++ Controller/EmailTemplateAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rj\EmailBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class EmailTemplateAdminController extends Controller
+{
+
+}
