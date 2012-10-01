843c2ab11d124cc499e4e87853981556ab6bb871
Jeremy Marc <jeremy.marc@me.com>
2012-09-30 20:46:09 -0700

    [Command] Change command name

--- Command/ImportEmailCommand.php
+++ Command/ImportEmailCommand.php
@@ -14,57 +14,57 @@ use Rj\EmailBundle\Entity\EmailTemplate;
  */
 class ImportEmailCommand extends ContainerAwareCommand
 {
-	/**
-	 * @see Command
-	 */
-	protected function configure()
-	{
-		$this
-			->setName('rj:email:import')
-			->setDescription('Import FOS Email into EmailTemplate')
-			;
-	}
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('rj:email:import-fosuserbundle')
+            ->setDescription('Import FOSUserbundle emails into EmailTemplate')
+            ;
+    }
 
-	/**
-	 * @see Command
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$emails = array('resetting', 'registration');
-		$langs = array('en', 'fr', 'de');
+    /**
+     * @see Command
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $emails = array('resetting', 'registration');
+        $langs = array('en', 'fr', 'de');
 
-		$container = $this->getApplication()->getKernel()->getContainer();
-		$em = $container->get('doctrine')->getEntityManager();
-		$translator = $container->get('translator');
-		$session = $container->get('session');
+        $container = $this->getApplication()->getKernel()->getContainer();
+        $em = $container->get('doctrine')->getEntityManager();
+        $translator = $container->get('translator');
+        $session = $container->get('session');
 
-		foreach($emails as $email) {
-			$template = new EmailTemplate();
-			$template->setName($email);
+        foreach($emails as $email) {
+            $template = new EmailTemplate();
+            $template->setName($email);
 
-			foreach($langs as $lang) {
-				$subject = $this->replaceTranslatorVariables($translator->trans($email . '.email.subject', array(), 'FOSUserBundle', $lang));
-				$body = $this->replaceTranslatorVariables($translator->trans($email . '.email.message', array(), 'FOSUserBundle', $lang));
+            foreach($langs as $lang) {
+                $subject = $this->replaceTranslatorVariables($translator->trans($email . '.email.subject', array(), 'FOSUserBundle', $lang));
+                $body = $this->replaceTranslatorVariables($translator->trans($email . '.email.message', array(), 'FOSUserBundle', $lang));
 
-				$template->translate($lang)
-					->setSubject($subject)
-					->setBody($body)
-					;
-			}
+                $template->translate($lang)
+                    ->setSubject($subject)
+                    ->setBody($body)
+                    ;
+            }
 
-			$em->persist($template);
-			$em->flush();
-		}
+            $em->persist($template);
+            $em->flush();
+        }
 
-		$output->writeln("All email templates have been imported.");
-	}
+        $output->writeln("All email templates have been imported.");
+    }
 
-	private function replaceTranslatorVariables($str)
-	{
-		//todo: REGEX /%([^%]+)%/
-		$str = str_replace('%username%', '{{ username }}', $str);
-		$str = str_replace('%confirmationUrl%', '{{ confirmationUrl }}', $str);
+    private function replaceTranslatorVariables($str)
+    {
+        //todo: REGEX /%([^%]+)%/
+        $str = str_replace('%username%', '{{ username }}', $str);
+        $str = str_replace('%confirmationUrl%', '{{ confirmationUrl }}', $str);
 
-		return $str;
-	}
+        return $str;
+    }
 }
