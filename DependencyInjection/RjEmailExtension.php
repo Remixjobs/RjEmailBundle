be0475a721c5fb42113a17ab62919d2bc4b87520
Jeremy Marc <jeremy.marc@me.com>
2013-03-03 14:15:12 -0800

    only load services.xml if FOSUSerBundle is loaded

--- DependencyInjection/RjEmailExtension.php
+++ DependencyInjection/RjEmailExtension.php
@@ -17,8 +17,6 @@ class RjEmailExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        //var_dump($container->getParameter('session.default_locale'));
-        //exit;
         $container->setParameter('rj_email.custom_loader', $config['custom_loader']);
         $container->setParameter('rj_email.default_locale', $config['default_locale']);
         $container->setParameter('rj_email.locales', $config['locales']);
@@ -29,8 +27,9 @@ class RjEmailExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
-        //todo: only if there is fosuserbundle
-        $loader->load('mailer.xml');
+        if (class_exists('FOS\UserBundle\Model\User')) {
+            $loader->load('mailer.xml');
+        }
 
         if (class_exists("Sonata\AdminBundle\Admin\Admin")) {
             $loader->load('sonata.xml');
